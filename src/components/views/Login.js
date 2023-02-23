f664a11f093c431e3bce3b3e097bc1bc22d5d273
theraffael <raffael.kummer@uzh.ch>
2023-02-23 22:17:31 +0100

    User Story 1 completed v2 :tada:

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {api, handleError} from 'helpers/api';
-import User from 'models/User';
+//import User from 'models/User';
 import {useHistory} from 'react-router-dom';
 import {Button} from 'components/ui/Button';
 import 'styles/views/Login.scss';
