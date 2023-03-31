99465278931a51e006f8eca29da728540fea6b40
yanmey <yanniclaurent.meyer@uzh.ch>
2023-03-31 09:58:36 +0200

    #23 create Find Lobby and Show all events button on the page home

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,10 +1,23 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { useHistory } from "react-router-dom";
 import BaseContainer from "components/ui/BaseContainer";
+import { Button } from "components/ui/Button";
 
 const Home = () => {
+  const history = useHistory();
+  const handleLobbiesClick = () => {
+    history.push("/Lobbies");
+  };
+
+  const handleEventsClick = () => {
+    history.push("/Events");
+  };
+
   return (
     <BaseContainer className="lobby">
       <h1>Home</h1>
+      <Button onClick={() => handleEventsClick()}>Show all Events</Button>
+      <Button onClick={() => handleLobbiesClick()}>Find Lobby</Button>
     </BaseContainer>
   );
 };
