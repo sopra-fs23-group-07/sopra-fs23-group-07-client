90adbc99b832a35f0566eabc4d5d3826d09c4605
oaboha <71384671+oaboha@users.noreply.github.com>
2023-04-01 10:46:52 +0200

    Added searchbar to the map

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useRef, useState, useReducer} from "react";
 import { useHistory } from "react-router-dom";
 import BaseContainer from "components/ui/BaseContainer";
 import { Button } from "components/ui/Button";
@@ -12,10 +12,39 @@ import ReactMapGL, {
 import mapboxgl from 'mapbox-gl';
 import route from "react-router-dom/es/Route";
 import "styles/ui/Map.scss";
+import Geocoder from "../../helpers/Geocoder";
 
 
 const AddLocation = () => {
     //const {state:{location:{lng, lat}}, dispatch} = useValue();
+
+    const [lng, setLng] = useState(8.541042);  //Longitude
+    const [lat, setLat] = useState(47.374449);  //Latitude
+
+
+    const mapRef = useRef();
+
+    /*
+This is used to retrieve the city of the user with the help of the ip adress
+ */
+    useEffect(() => {
+        if (!lng && !lat) {
+            fetch('https://ipapi.co/json')
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    mapRef.current.flyTo({
+                        center: [data.longitude, data.latitude],
+                    });
+                    // dispatch({
+                    //     type: 'UPDATE_LOCATION',
+                    //     payload: { lng: data.longitude, lat: data.latitude },
+                    // });
+                });
+        }
+    }, []);
+
     return (
         <Box
             sx={{
@@ -23,26 +52,39 @@ const AddLocation = () => {
                 position:'relative'
             }}>
             <ReactMapGL
+                ref={mapRef}
                 mapboxAccessToken={process.env.REACT_APP_MAP_TOKEN}
                 initialViewState={{
-                    longitude: -100,
-                    latitude: 40,
-                    zoom: 3.5,
+                    longitude: lng,
+                    latitude: lat,
+                    zoom: 12,
                 }}
                 mapStyle="mapbox://styles/mapbox/streets-v11"
             >
 
-                <Marker
-                    latitude={0}
-                    longitude={0}
-                    draggable
-                    onDragEnd={(e) =>
-                        dispatch({
-                            type: 'UPDATE_LOCATION',
-                            payload: { lng: e.lngLat.lng, lat: e.lngLat.lat },
-                        })
-                    }
+                {/*<Marker //initial state of the marker*/}
+                {/*    latitude={lng}*/}
+                {/*    longitude={lat}*/}
+                {/*    draggable*/}
+                {/*    // onDragEnd={(e) =>*/}
+                {/*    //     dispatch({*/}
+                {/*    //         type: 'UPDATE_LOCATION',*/}
+                {/*    //         payload: { lng: e.lngLat.lng, lat: e.lngLat.lat },*/}
+                {/*    //     })*/}
+                {/*   // }*/}
+                {/*/>*/}
+                <NavigationControl position="bottom-right"/> //this is used for the zoom in and zoom out buttons
+                <GeolocateControl //this allows to track the position of the user. With the track icon.
+                    position="top-left"
+                    trackUserLocation
+                    // onGeolocate={(e) =>
+                    //     dispatch({
+                    //         type: 'UPDATE_LOCATION',
+                    //         payload: { lng: e.coords.longitude, lat: e.coords.latitude },
+                    //     })
+                    // }
                 />
+                <Geocoder />
             </ReactMapGL>
             {/*<div ref={mapContainer} className="map-container" />*/}
 
