ae4a6e1deb1eb8000def705802fc29e731c6a49c
oaboha <71384671+oaboha@users.noreply.github.com>
2023-04-01 10:01:11 +0200

    Small fix
    Changing eslintr was not needed

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -9,7 +9,7 @@ import ReactMapGL, {
     Marker,
     NavigationControl,
 } from 'react-map-gl';
-import mapboxgl from '!mapbox-gl';
+import mapboxgl from 'mapbox-gl';
 import route from "react-router-dom/es/Route";
 import "styles/ui/Map.scss";
 
@@ -63,32 +63,32 @@ const Home = () => {
     history.push("/Events");
   };
 
-    // const mapContainer = useRef(null);       //MapBox Container
-    // const map = useRef(null);                //MapBox rendered element
-    // const [lng, setLng] = useState(77.378);  //Longitude
-    // const [lat, setLat] = useState(28.624);  //Latitude
-    // const [zoom, setZoom] = useState(12);    //Zoom Level
-    // const start = [lng, lat];                //Initial Directions
-    //
-    // // mapboxgl.accessToken = process.env.REACT_APP_MAP_TOKEN;
-    // //
-    // //
-    // // useEffect(() => {
-    // //     if (map.current) return; // initialize map only once
-    // //     map.current = new mapboxgl.Map({
-    // //         container: mapContainer.current,
-    // //         style: 'mapbox://styles/mapbox/streets-v11',
-    // //         center: [lng, lat],
-    // //         zoom: zoom
-    // //     });
-    // //     map.current.on('move', () => {
-    // //         setLng(map.current.getCenter().lng.toFixed(4));
-    // //         setLat(map.current.getCenter().lat.toFixed(4));
-    // //         setZoom(map.current.getZoom().toFixed(2));
-    // //     });
-    // //     //route();
-    // //
-    // // }, [map.current]);
+    const mapContainer = useRef(null);       //MapBox Container
+    const map = useRef(null);                //MapBox rendered element
+    const [lng, setLng] = useState(77.378);  //Longitude
+    const [lat, setLat] = useState(28.624);  //Latitude
+    const [zoom, setZoom] = useState(12);    //Zoom Level
+    const start = [lng, lat];                //Initial Directions
+
+    mapboxgl.accessToken = process.env.REACT_APP_MAP_TOKEN;
+
+
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom
+        });
+        map.current.on('move', () => {
+            setLng(map.current.getCenter().lng.toFixed(4));
+            setLat(map.current.getCenter().lat.toFixed(4));
+            setZoom(map.current.getZoom().toFixed(2));
+        });
+        //route();
+
+    }, [map.current]);
 
   return (
     <BaseContainer className="lobby">
@@ -96,9 +96,9 @@ const Home = () => {
 
       <Button onClick={() => handleEventsClick()}>Show all Events</Button>
       <Button onClick={() => handleLobbiesClick()}>Find Lobby</Button>
-        {/*<div id="map"></div>*/}
-        {/*<div ref={mapContainer} className="map-container" />*/}
-        <AddLocation></AddLocation>
+        <div id="map"></div>
+        <div ref={mapContainer} className="map-container" />
+        {/*<AddLocation></AddLocation>*/}
     </BaseContainer>
   );
 };
