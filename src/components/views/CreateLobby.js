5297b4e01f69ca2e1a41f16853e9a5c9126674f3
theraffael <raffael.kummer@uzh.ch>
2023-04-07 14:26:27 +0200

    - imporved layout of CreateLobby page #22

--- src/components/views/CreateLobby.js
+++ src/components/views/CreateLobby.js
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import BaseContainer from "components/ui/BaseContainer";
 import { useHistory } from "react-router-dom";
 import { api, handleError } from "helpers/api";
+import InputLabel from '@mui/material/InputLabel';
+import {Button, Select, MenuItem, FormControl, Box} from "@mui/material";
+import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
+import 'styles/views/CreateLobby.scss'
+
 
 // On this page the host chooses different attributes of his lobby
 // TODO: Make sure that only loggedin users can open this page. (Guard)
@@ -54,68 +59,81 @@ const CreateLobby = () => {
 
   // displays the three input fields
   return (
-    <BaseContainer className="game container">
+    <BaseContainer className="createLobby container">
       <h1>Create a new Lobby </h1>
-      <div className="LobbyNameSelection">
+      <div className="createLobby label">
         <h3>Select lobby name:</h3>
         <input
-          type="text"
-          id="lobbyName"
-          value={lobbyName}
-          onChange={(e) => setLobbyName(e.target.value)}
+            className="createLobby input"
+            type="text"
+            id="lobbyName"
+            placeholder="Enter Lobby Name"
+            value={lobbyName}
+            onChange={(e) => setLobbyName(e.target.value)}
         />
       </div>
-      <br />
-      <div className="ParticipantsMaxSelection" style={{textAlign: 'center'}}>
+      <div className="createLobby label">
         <h3> Enter maximum number of participants:</h3>
         <input
-          type="text"
-          id="maxParticipants"
-          value={maxParticipants}
-          onChange={(e) => setMaxParticipants(e.target.value)}
+            className="createLobby input"
+            type="text"
+            id="maxParticipants"
+              placeholder="Enter Maximum Number of Participants"
+
+              value={maxParticipants}
+            onChange={(e) => setMaxParticipants(e.target.value)}
         />
       </div>
-      <br />
-      <div className="RegionSelection">
+      <div className="createLobby label">
         <h3>Choose Region:</h3>
-        <select
-          id="region"
-          value={region}
-          onChange={(e) => setRegion(e.target.value)}
-        >
-          <option value="" selected disabled>
-            Choose a canton
-          </option>
-          <option value="AG">Aargau</option>
-          <option value="AI">Appenzell Innerrhoden</option>
-          <option value="AR">Appenzell Ausserrhoden</option>
-          <option value="BE">Bern</option>
-          <option value="BL">Basel-Landschaft</option>
-          <option value="BS">Basel-Stadt</option>
-          <option value="FR">Fribourg</option>
-          <option value="GE">Geneva</option>
-          <option value="GL">Glarus</option>
-          <option value="GR">Graubünden</option>
-          <option value="JU">Jura</option>
-          <option value="LU">Luzern</option>
-          <option value="NE">Neuchâtel</option>
-          <option value="NW">Nidwalden</option>
-          <option value="OW">Obwalden</option>
-          <option value="SG">St. Gallen</option>
-          <option value="SH">Schaffhausen</option>
-          <option value="SO">Solothurn</option>
-          <option value="SZ">Schwyz</option>
-          <option value="TG">Thurgau</option>
-          <option value="TI">Ticino</option>
-          <option value="UR">Uri</option>
-          <option value="VD">Vaud</option>
-          <option value="VS">Valais</option>
-          <option value="ZG">Zug</option>
-          <option value="ZH">Zürich</option>
-        </select>
+        <Box sx={{ minWidth: 240 }}>
+          <FormControl fullWidth>
+            <Select
+                className="createLobby input"
+                labelId="createLobby-select"
+                id="createLobby-select"
+                value={region}
+                label="Region"
+                onChange={(e) => setRegion(e.target.value)}
+            >
+              <MenuItem value="AG">Aargau</MenuItem>
+              <MenuItem value="AI">Appenzell Innerrhoden</MenuItem>
+              <MenuItem value="AR">Appenzell Ausserrhoden</MenuItem>
+              <MenuItem value="BE">Bern</MenuItem>
+              <MenuItem value="BL">Basel-Landschaft</MenuItem>
+              <MenuItem value="BS">Basel-Stadt</MenuItem>
+              <MenuItem value="FR">Fribourg</MenuItem>
+              <MenuItem value="GE">Geneva</MenuItem>
+              <MenuItem value="GL">Glarus</MenuItem>
+              <MenuItem value="GR">Graubünden</MenuItem>
+              <MenuItem value="JU">Jura</MenuItem>
+              <MenuItem value="LU">Luzern</MenuItem>
+              <MenuItem value="NE">Neuchâtel</MenuItem>
+              <MenuItem value="NW">Nidwalden</MenuItem>
+              <MenuItem value="OW">Obwalden</MenuItem>
+              <MenuItem value="SG">St. Gallen</MenuItem>
+              <MenuItem value="SH">Schaffhausen</MenuItem>
+              <MenuItem value="SO">Solothurn</MenuItem>
+              <MenuItem value="SZ">Schwyz</MenuItem>
+              <MenuItem value="TG">Thurgau</MenuItem>
+              <MenuItem value="TI">Ticino</MenuItem>
+              <MenuItem value="UR">Uri</MenuItem>
+              <MenuItem value="VD">Vaud</MenuItem>
+              <MenuItem value="VS">Valais</MenuItem>
+              <MenuItem value="ZG">Zug</MenuItem>
+              <MenuItem value="ZH">Zürich</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
       </div>
-      <br />
-      <button onClick={() => handleLobbyClick()}>Create Lobby</button>
+      <div className="createLobby button-container">
+      <Button
+          onClick={() => handleLobbyClick()}
+          variant="contained"
+      startIcon={<AddBoxOutlinedIcon/>}>
+        Create Lobby
+      </Button>
+      </div>
     </BaseContainer>
   );
 };
