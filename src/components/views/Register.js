94d4a9407d6322e72fb0802a56e3de62e5a946a4
theraffael <raffael.kummer@uzh.ch>
2023-04-14 19:37:36 +0200

    updated Layout of Login and Register Page

--- src/components/views/Register.js
+++ src/components/views/Register.js
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import { api, handleError } from "helpers/api";
 import User from "models/User";
 import { useHistory } from "react-router-dom";
-import { Button } from "components/ui/Button";
 import "styles/views/Register.scss";
 import BaseContainer from "components/ui/BaseContainer";
-import PropTypes from "prop-types";
+import {Box, Grid, Paper, TextField, Typography, Button} from "@mui/material";
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import LoginIcon from '@mui/icons-material/Login';
 
 /*
 It is possible to add multiple components inside a single file,
@@ -13,31 +14,24 @@ however be sure not to clutter your files with an endless amount!
 As a rule of thumb, use one file per component and only add small,
 specific components that belong to the main one in the same file.
  */
-const FormField = (props) => {
-  return (
-    <div className="register field">
-      <label className="register label">{props.label}</label>
-      <input
-        className="register input"
-        placeholder="enter here.."
-        value={props.value}
-        onChange={(e) => props.onChange(e.target.value)}
-      />
-    </div>
-  );
-};
 
-FormField.propTypes = {
-  label: PropTypes.string,
-  value: PropTypes.string,
-  onChange: PropTypes.func,
-};
-
-const Register = (props) => {
+const Register = () => {
   const history = useHistory();
-  const [username, setUsername] = useState(null);
-  const [password, setPassword] = useState(null);
-  const [email , setEmail] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email , setEmail] = useState("");
+
+  const handleUsernameInputChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleEmailInputChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordInputChange = (event) => {
+    setPassword(event.target.value);
+  };
 
   const doRegister = async () => {
     try {
@@ -58,48 +52,69 @@ const Register = (props) => {
     }
   };
 
+
   return (
     <BaseContainer>
-      <div className="register container">
-        <div className="register form">
-          <FormField
-            label="Username"
-            value={username}
-            onChange={(un) => setUsername(un)}
-          />
-          <FormField
-              label="Email"
+      <Grid item xs={12} sx={{paddingY: 2, maxWidth: 800, m:'0 auto'}}>
+        <Typography variant={"h3"} >Register</Typography>
+      </Grid>
+        <Paper
+            sx={{
+              paddingY: 10,
+              paddingX:4,
+              mt: 2,
+              maxWidth: 800,
+              flexGrow: 1,
+              margin : '0 auto'
+            }}
+        >
+          <Box sx={{ display: 'flex', flexDirection: 'column',width: '60%', margin: '0 auto'}}>
+            <TextField
+                label={"Username"}
+                placeholder={"Enter your username"}
+                type={"text"}
+                value={username}
+                onChange={handleUsernameInputChange}
+                sx={{ mt : 2 }}
+            />
+            <TextField
+              label={"Email"}
+              placeholder={"Enter your email"}
+              type={"email"}
               value={email}
-              onChange={(e) => setEmail(e)}
-          />
-          <FormField
-            label="Password"
-            value={password}
-            onChange={(pw) => setPassword(pw)}
-          />
-          <div className="register button-container">
-            <Button
-              disabled={!username || !email || !password}
-              width="100%"
-              onClick={() => doRegister()}
+              onChange={handleEmailInputChange}
+              sx={{ mt : 2 }}
+              />
+            <TextField
+              label={"Password"}
+              placeholder={"Enter your password"}
+              type={"password"}
+              value={password}
+              onChange={handlePasswordInputChange}
+              sx={{ mt : 2 }}
+            />
+            <Button variant={"contained"}
+                    startIcon={<AppRegistrationIcon/>}
+                    disabled={!username || !email || !password}
+                    onClick={() => doRegister()}
+                    sx={{ marginY: 2, paddingY:2, paddingX:4, justifySelf: 'center', alignSelf: 'center' }}
             >
-              &#x1F511; Register
+              Register
             </Button>
-          </div>
+
           <div>Already a User? Please go to the Login Page.</div>
-          <div className="register button-container">
-            <Button width="100%" onClick={() => history.push("/login")}>
-              &#x1F513; Login
+
+            <Button variant={"contained"}
+                           startIcon={<LoginIcon/>}
+                           onClick={() => history.push("/login")}
+                           sx={{ mt: 2, paddingY:2, paddingX:4, justifySelf: 'center', alignSelf: 'center' }}
+            >
+              Login
             </Button>
-          </div>
-        </div>
-      </div>
+          </Box>
+        </Paper>
     </BaseContainer>
   );
 };
 
-/**
- * You can get access to the history object's properties via the withRouter.
- * withRouter will pass updated match, location, and history props to the wrapped component whenever it renders.
- */
 export default Register;
