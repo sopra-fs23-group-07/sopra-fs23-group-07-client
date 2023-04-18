958e2487885c8eb99012d4d6b0fa6d65d3dae8e4
yanmey <yanniclaurent.meyer@uzh.ch>
2023-04-18 19:06:18 +0200

    not available message for the Myevents page closes #61

--- src/components/views/MyEvents.js
+++ src/components/views/MyEvents.js
@@ -1,16 +1,22 @@
 import React from "react";
 import BaseContainer from "components/ui/BaseContainer";
-import {Grid, Typography} from "@mui/material";
-
+import { Grid, Typography } from "@mui/material";
 
 const MyEvents = () => {
-    return (
-        <BaseContainer className="lobby">
-            <Grid item xs={12}>
-                <Typography variant={'h3'}>My Events</Typography>
-            </Grid>
-        </BaseContainer>
-    );
+  return (
+    <BaseContainer className="lobby">
+      <Grid item xs={12}>
+        <Typography variant={"h3"}>My Events</Typography>
+        <Typography
+          variant={"h5"}
+          style={{ color: "#333", textAlign: "center", marginTop: "200px" }}
+        >
+          Coming soon! This feature will be available soon. Thank you for your
+          patience!
+        </Typography>
+      </Grid>
+    </BaseContainer>
+  );
 };
 
 export default MyEvents;
