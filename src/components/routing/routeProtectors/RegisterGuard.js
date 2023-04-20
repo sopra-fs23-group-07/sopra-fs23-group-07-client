bc5f0915ff4e3e3e953296963585e8ca9b09ff98
oaboha <71384671+oaboha@users.noreply.github.com>
2023-04-20 19:02:37 +0200

    If server is down - Guard sends request and if server down it will remove localStorage

--- src/components/routing/routeProtectors/RegisterGuard.js
+++ src/components/routing/routeProtectors/RegisterGuard.js
@@ -1,19 +1,49 @@
 import {Redirect} from "react-router-dom";
 import PropTypes from "prop-types";
+import {api} from "../../../helpers/api";
+import {useEffect, useState} from "react";
+import {Spinner} from "../../ui/Spinner";
 
-/**
- *
- * Another way to export directly your functional component.
- */
 export const RegisterGuard = props => {
-  if (!localStorage.getItem("token")) {
-    return props.children;
-  }
-  // if user is already logged in, redirects to the main /app
-  return <Redirect to="/Home"/>;
+    const userId = localStorage.getItem("userId");
+    const token = localStorage.getItem("token");
+    const [TokenServer, setTokenServer] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                await api.get(`/users/${userId}`);
+                setTokenServer(true);
+                console.log("success", TokenServer);
+            } catch (error) {
+                setTokenServer(false);
+                console.log("error", TokenServer);
+            }
+        };
+        fetchData();
+    }, []);
+    //
+    // useEffect(() => {
+    //     console.log("TokenServer changed:", TokenServer);
+    // }, [TokenServer]);
+
+    if (!localStorage.getItem("token")) {
+        return props.children;
+    }
+
+    if (localStorage.getItem("token")) {
+        if (TokenServer === true) {
+            return <Redirect to="/Home"/>;
+        } else if (TokenServer === false) {
+            localStorage.clear();
+            return props.children;
+
+        }
+        return <Spinner></Spinner>;
+    }
 
 };
 
 RegisterGuard.propTypes = {
-  children: PropTypes.node
-}
+    children: PropTypes.node
+};
