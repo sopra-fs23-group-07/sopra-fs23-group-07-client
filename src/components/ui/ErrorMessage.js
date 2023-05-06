cfc8931a19d034cc85731f7b94bf165d2a63d4d5
theraffael <raffael.kummer@uzh.ch>
2023-05-06 13:08:52 +0200

    error messages are now displayed with Toastify. Additionally, the user now only sees the error message (without url, error code, etc). closes #90

--- src/components/ui/ErrorMessage.js
+++ src/components/ui/ErrorMessage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const ErrorMessage = ({ error, onClose }) => {
+const ErrorMessage = ({ error }) => {
     if (!error) return null;
 
     // Call toast with the error message or object
