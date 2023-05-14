1801e52c94288e618639ce8269f13aa784a33a37
oaboha <71384671+oaboha@users.noreply.github.com>
2023-05-14 19:35:51 +0200

    Check for token when accessing Profile to prevent relying on userId only

--- src/components/routing/routeProtectors/ProfileGuard.js
+++ src/components/routing/routeProtectors/ProfileGuard.js
@@ -1,16 +1,50 @@
-import { Redirect } from "react-router-dom";
+import {Redirect, useHistory} from "react-router-dom";
 import PropTypes from "prop-types";
+import React, {useEffect} from "react";
+import {api, handleError} from "../../../helpers/api";
+import {toast} from "react-toastify";
 
 export const ProfileGuard = (props) => {
-  if (
-    localStorage.getItem("token") !== "null" &&
-    localStorage.getItem("token")
-  ) {
-    return props.children;
-  }
-  return <Redirect to="/login" />;
+
+
+    const userId = localStorage.getItem("userId");
+    const token = localStorage.getItem("token");
+    const history = useHistory();
+
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+
+                const response = await api.get(`/users/${userId}`);
+                if (token != response.data.token) {
+
+                    history.push("/Login");
+                    // return <Login lobby="true" lobbyId={lobbyId} />;
+                } else {
+                    return props.children;
+                }
+
+
+            } catch (error) {
+                toast.error(handleError(error));
+            }
+        }
+
+        fetchData();
+
+
+    }, []);
+
+    if (
+        localStorage.getItem("token") !== "null" &&
+        localStorage.getItem("token")
+    ) {
+        return props.children;
+    }
+    return <Redirect to="/login"/>;
 };
 
 ProfileGuard.propTypes = {
-  children: PropTypes.node,
+    children: PropTypes.node,
 };
