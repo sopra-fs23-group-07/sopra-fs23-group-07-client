fe7fd8731501d1a4b241325002e981a814ec7165
Suleman <sulekhan111@gmail.com>
2023-05-16 10:41:03 +0200

    closes #137, login guard check token and id if they are correct

--- src/components/routing/routeProtectors/LoginGuard.js
+++ src/components/routing/routeProtectors/LoginGuard.js
@@ -1,7 +1,9 @@
-import {Redirect} from "react-router-dom";
+import {Redirect, useHistory} from "react-router-dom";
 import PropTypes from "prop-types";
 import React, {useEffect, useState} from "react";
 import {api} from "helpers/api.js";
+import Spinner from "components/ui/Spinner";
+
 
 /**
  *
@@ -10,18 +12,42 @@ import {api} from "helpers/api.js";
 export const LoginGuard = props => {
 
     const userId = localStorage.getItem("userId");
-    const token = localStorage.getItem("token");
+        const token = localStorage.getItem("token");
+        console.log(userId);
+        console.log(token);
+        const [toLogin, setToLogin] = useState(false);
 
-    // if user is already logged in, redirects to the main /app
-    if (userId && token) {
-        return <Redirect to="/Home"/>;
-    } else {
-        localStorage.removeItem("token");
-        localStorage.removeItem("userId");
-        return props.children;
+        const history = useHistory();
+
+        //useEffect(() => {
+            async function fetchData() {
+              try {
+                const response = await api.get(`/users/${userId}`);
+                console.log(response.data.token);
+
+                if(token === response.data.token) {
+                    history.push("/Home"); }
+                else {
+                    localStorage.removeItem("token");
+                    localStorage.removeItem("userId");
+                    setToLogin(true);}
+
+
+              } catch (error) {
+                localStorage.removeItem("token");
+                localStorage.removeItem("userId");
+                console.log(error);
+                setToLogin(true);
+              }
+            }
+
+            fetchData();
+          //}, []);
+        if(setToLogin) { return props.children; }
+        return <Spinner />
 
 };
 
 LoginGuard.propTypes = {
     children: PropTypes.node
-}}
+}
