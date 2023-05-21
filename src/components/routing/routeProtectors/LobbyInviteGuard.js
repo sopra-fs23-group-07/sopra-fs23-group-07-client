860303e61c13b59ea47d433c48d9a38b14dce2bd
Suleman <sulekhan111@gmail.com>
2023-05-21 14:25:44 +0200

    fixes #155, possible fix for bug in inviteLobbyGuard

--- src/components/routing/routeProtectors/LobbyInviteGuard.js
+++ src/components/routing/routeProtectors/LobbyInviteGuard.js
@@ -78,7 +78,7 @@ export const LobbyInviteGuard = props => {
         }
     }
 
-    fetchData().then(() => console.log("fetchData was called"));
+    fetchData().catch(err => console.log(err));
 
 
 
